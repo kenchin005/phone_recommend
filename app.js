4c5abac47c28bc8bcb668e784b238b6441d4a87a
kenchin005 <60567898+kenchin005@users.noreply.github.com>
2020-11-16 23:21:07 +0900

    1116

--- app.js
+++ app.js
@@ -60,5 +60,12 @@ app.post('/create',(req,res)=>{
   })
 });
 
+
+//管理データ削除
+app.post('/delete',(req,res)=>{
+  
+})
+
+
 //サーバーを起動したら、リクエストを8000番ポートで待ち受ける設定。
 app.listen(8000, () => console.log('Example app listening on port 8000!'))
