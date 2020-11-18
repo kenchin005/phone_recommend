bc08b51724c2e4cc7b6b7ba091740356ffd832d8
kenchin005 <60567898+kenchin005@users.noreply.github.com>
2020-11-19 02:26:16 +0900

    1119

--- app.js
+++ app.js
@@ -1,70 +1,71 @@
 //expressモジュールを使えるように設定
-const express = require('express')
+const express = require('express');
 //expressモジュールを利用しアプリケーションオブジェクトappを作成
-const app = express()
+const app = express();
 
 //SQL接続
-const mysql = require('mysql');
+// const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'phone_db'
-});
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//  password: '',
+//  database: 'phone_db'
+// });
 
-//接続できなかったらエラーを返す
-connection.connect((err) => {
-  if (err) {
-    console.log('error connecting: ' + err.stack);
-    return;
-  }
-  console.log('success');
-});
+// 接続できなかったらエラーを返す
+// connection.connect((err) => {
+//  if (err) {
+//    console.log('error connecting: ' + err.stack);
+//    return;
+//  }
+//  console.log('success');
+// });
 
 
 //フォーム取得
-app.use(express.urlencoded({extended: false}));
+// app.use(express.urlencoded({extended: false}));
 
 
 
 app.use(express.static('public'));
+app.use(express.static('views'));
 
 
 // 最初の画面アクセス
-app.get('/index',(req,res)=>{
+app.get('/',(req,res)=>{
   res.render('index.ejs')
 });
 
 
 //list
-app.get('/phone_list', (req, res) => {
-  connection.query('SELECT*FROM items',(error,results)=>{
-    res.render('phone_list.ejs',{items: results})
-  })
-});
+// app.get('/phone_list', (req, res) => {
+//   connection.query('SELECT*FROM items',(error,results)=>{
+//     res.render('phone_list.ejs',{items: results})
+//   })
+// });
 
 //リストから新規機種登録
-app.get('/new',(req,res)=>{
-  res.render('new.ejs')
-})
+// app.get('/new',(req,res)=>{
+//   res.render('new.ejs')
+// })
 
-app.post('/create',(req,res)=>{
-  connection.query('INSERT INTO items(pict,phoneName,size,gaso,battery,IPX,movieSize,other,Recommend)VALUES(?)',
-  [req.body.pict,phoneName,size,gaso,battery,IPX,movieSize,other,Recommend],
-  (error,results)=>{
+// app.post('/create',(req,res)=>{
+//   connection.query('INSERT INTO items(pict,phoneName,size,gaso,battery,IPX,movieSize,other,Recommend)VALUES(?)',
+//   [req.body.pict,phoneName,size,gaso,battery,IPX,movieSize,other,Recommend],
+//   (error,results)=>{
    
-      res.redirect('/phone_list');
+//       res.redirect('/phone_list');
     
     
-  })
-});
+//   })
+// });
 
 
 //管理データ削除
-app.post('/delete:id',(req,res)=>{
-  res.redirect('/phone_list')
-})
+// app.post('/delete:id',(req,res)=>{
+//   res.redirect('/phone_list')
+// })
 
 
 //サーバーを起動したら、リクエストを8000番ポートで待ち受ける設定。
