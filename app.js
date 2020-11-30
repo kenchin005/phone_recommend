ca349123fba411497bb99e2eb4c8ac9d1d426a73
kenchin005 <60567898+kenchin005@users.noreply.github.com>
2020-12-01 00:31:46 +0900

    1130

--- app.js
+++ app.js
@@ -112,10 +112,16 @@ app.post('/uploads',(req,res)=>{
 // });
 
 //管理データ削除テスト
-app.post('/delete:id',(req,res)=>{
-    console.log(req.params.id);
+app.post('/delete/:id',(req,res)=>{
+
+  var id = req.params.id
+
+  connection.query('DELETE FROM test_tebale WHERE id = ?',
+  [id],(error,results)=>{
     res.redirect('/phone_list');
-})
+  })
+});
+
 
 
 //管理データ削除
